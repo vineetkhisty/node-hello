ed557ab3f827b6b82a7a1152fc8cc9d8d2a7f0f6
Vineet Khisty <74192508+vineetkhisty@users.noreply.github.com>
2025-05-29 14:59:02 +0530

    Update index.js
    Corrected the code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hi Vineet! Welcome to Node App\n'
+  const msg = 'Hello Vineet! Welcome to Node App\n'
   res.end(msg); 
 });
 
